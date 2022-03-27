d02453cd161cfd7b5af7aa86c39dcc185b6986a2
unknown <minwoo21>
2022-03-27 18:28:30 +0900

    Initial commit

---
+++ src/main/java/com/example/logbackDemo/config/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package com.example.logbackDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+* @package : com.example.logbackDemo.config
+* @name : SwaggerConfiguration.java
+* @date : 2022-03-27 오후 4:34
+* @author : Flature
+* @version : 1.0.0
+**/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.example.logbackDemo"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("logbackDemo Open API Test with Swagger")
+            .description("설명 부분")
+            .version("1.0.0")
+            .build();
+    }
+}
