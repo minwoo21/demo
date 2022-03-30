195e47d108f3994a2756efa123d4a4721abbd2b9
minwoo21 <alsdnrla21@gmail.com>
2022-03-30 16:18:24 +0900

    clean code

--- src/main/java/com/example/logbackDemo/controller/HelloController.java
+++ src/main/java/com/example/logbackDemo/controller/HelloController.java
@@ -16,11 +16,6 @@ public class HelloController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-    }
-
     @PostMapping("log-test")
     public void logTest(){
         long startTime = System.currentTimeMillis();
@@ -39,30 +34,5 @@ public class HelloController {
         LOGGER.info( "[getProduct] Response :: {} Response Time = {}ms",Parm,(System.currentTimeMillis() - startTime));
     }
 
-
-
-
-
-    @PostMapping("/exception")
-    public void exceptionTest() throws Exception {
-        throw new Exception();
-    }
-
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<Map<String, String>> ExceptionHandler(Exception e) {
-        HttpHeaders responseHeaders = new HttpHeaders();
-        //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json");
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
-
-        LOGGER.info(e.getMessage());
-        LOGGER.info("Controller 내 ExceptionHandler 호출");
-
-        Map<String, String> map = new HashMap<>();
-        map.put("error type", httpStatus.getReasonPhrase());
-        map.put("code", "400");
-        map.put("message", "에러 발생");
-
-        return new ResponseEntity<>(map, responseHeaders, httpStatus);
-    }
 
 }
